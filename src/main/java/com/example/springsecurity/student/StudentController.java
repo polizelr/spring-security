225f977cf4b559ae91214957702d58a64fec3d74
Rafaela Polizel <polizelc.rafaela@gmail.com>
2021-01-17 22:02:58 -0300

    Adding Student entity

---
+++ src/main/java/com/example/springsecurity/student/StudentController.java
@@ -0,0 +1,18 @@
+package com.example.springsecurity.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+
+    @GetMapping(path = "{studentId}")
+    public Student getStudent(@PathVariable("studentId") Integer studentId){
+
+    }
+
+
+}
